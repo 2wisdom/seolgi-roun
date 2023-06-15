340883cc02520696a03f811c9093b0ee8d797478
Lizzie <leejihye7117@gmail.com>
2023-06-16 01:39:46 +0900

    add banner

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,7 +1,9 @@
+import Banner from "../components/banner";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <Banner />
     </main>
   );
 }
---
+++ src/app/components/banner.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function Banner() {
+  return (
+    <Wrapper>
+      <Link href="/">
+        <Image src="/banner_logo.svg" alt="banner" width={433} height={212} />
+      </Link>
+    </Wrapper>
+  );
+}
