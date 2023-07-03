8592cec24363b05f8cea88a671d27c32d4cdd42d
Lizzie <leejihye7117@gmail.com>
2023-07-03 16:39:19 +0900

    GlobalWrapper 스타일 추가

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,12 +2,15 @@ import { Typography } from "@mui/material";
 import Banner from "../components/Banner";
 import ItemCard from "../components/ItemCard";
 import Link from "next/link";
+import { GlobalWrapper } from "../style/GlobalWrapper";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <ItemCard />
+      <GlobalWrapper>
+        <ItemCard />
+      </GlobalWrapper>
     </main>
   );
 }
---
+++ src/app/style/GlobalWrapper.ts
@@ -0,0 +1,10 @@
+"use client";
+import styled from "@emotion/styled";
+
+export const GlobalWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  align-items: center;
+  max-width: 1200px;
+`;
